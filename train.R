31397448d13ea24cbe9d9d47f51347f90ae61838
leojo <leojohannsson91@gmail.com>
2015-11-20 16:16:46 +0000

    Fine tuning in progress

--- train.R
+++ train.R
@@ -1,4 +1,4 @@
-learnIcelandic <- function(csvDirPath){
+learnIcelandic <- function(csvDirPath,verbose=FALSE){
   require(tm)
   require(stylo)
   
@@ -8,22 +8,22 @@ learnIcelandic <- function(csvDirPath){
   words <- c()
   lemmas <- c()
   for(fileName in filePaths){
-    print(paste("Reading file",fileName,"..."))
+    if(verbose)print(paste("Reading file",fileName,"..."))
     rawData <- read.csv(fileName, fileEncoding="UTF-8")
-    print("done!")
+    if(verbose)print("done!")
     
-    print("Gathering words...")
+    if(verbose)print("Gathering words...")
     trim1 <- rawData$Word[!grepl("[^[:alpha:]]",rawData$Word)]
     trim2 <- trim1[trim1!=""]
     words <- c(words,tolower(as.character(trim2)))
-    print("done!")
+    if(verbose)print("done!")
     
     
-    print("Gathering lemmas...")
+    if(verbose)print("Gathering lemmas...")
     trim1 <- rawData$Lemma[!grepl("[^[:alpha:]]",rawData$Lemma)]
     trim2 <- trim1[trim1!=""]
     lemmas <- c(lemmas,tolower(as.character(trim2)))
-    print("done!")
+    if(verbose)print("done!")
   }
   
   dict <<- table(words) # Get a unique list of words and their frequency, alphabetized
@@ -39,83 +39,33 @@ learnIcelandic <- function(csvDirPath){
   lemmasDict <<- table(lemmas)
 }
 
-#
-find = function(thing, dict) {
-  return (length(dict[grepl(paste("^",thing,"$", sep = ""), names(dict))]))
-}
-
 # Function that calculates the probability of a lemma
-P_lemma = function(l) {
-  freq <- as.numeric(lemmasDict[l])+1 #Frequency of lemma 'l' in the dictionary.
-  if(is.na(freq)) freq <- 1 # set as 1 if it doesn't appear anywhere
+P_lemma = function(lemma) {
+  n <- 10
+  if(nchar(lemma)==1) n <- 10000
+  pL <- max(min(lemmasDict[tolower(lemma)]/n,0.9),0.1)
+  if(is.na(pL)) pL <- 0.1
   
-  # Calculate the probability of the word:
-  return (freq/as.numeric(max(lemmasDict)+1))
-}
-
-# Function that calculates the probability of a word
-P_word = function(w) {
-  freq <- as.numeric(dict[w])+1 #Frequency of lemma 'l' in the dictionary.
-  if(is.na(freq)) freq <- 1 # set as 1 if it doesn't appear anywhere
-  
-  # Calculate the probability of the word:
-  return (freq/as.numeric(max(dict)+1))
+  return(pL)
 }
 
 # Function that calculates the probability that a 
 # B comes after A in a sentence
-P_bigram_lemma_AX <- function(A, B) {
+P_B_after_A <- function(A, B) {
+  n <- 100
   bigram <- paste(A,B)
-  freq <<- as.numeric(bigramDict[bigram])+1
-  if(is.na(freq)) freq <- 1
-  return(max(min(freq/10,0.9),0.1))
-}
-
-# Function that calculates the probability that a 
-# B comes after A in a sentence
-P_bigram_AX <- function(A, B) {
-  bigram <- paste(A,B)
-  freq <<- as.numeric(wordBigramDict[bigram])+1
-  if(is.na(freq)) freq <- 1
-  freqAX <<- as.numeric(sum(wordBigramDict[grepl(paste("^",A,sep=""),names(wordBigramDict))]))+1
-  if(is.na(freqAX)) freqAX <- 2
-  return(freq/freqAX)
-}
-
-# Function that calculates the probability that a 
-# B comes after A in a sentence
-P_bigram_XC <- function(B, C) {
-  bigram <- paste(B,C)
-  freq <- as.numeric(wordBigramDict[bigram])+1
-  if(is.na(freq)) freq <- 1
-  freqXC <- as.numeric(sum(wordBigramDict[grepl(paste(C,"$",sep=""),names(wordBigramDict))]))+1
-  if(is.na(freqXC)) freqXC <- 2
-  return(freq/freqXC)
-}
-
-
-# Function that calculates the normalized probability that B comes after A
-P_trigram_norm <- function(A, B, C, verbose=FALSE){
-  print("Calculating...")
-  pAB <- P_bigram_AX(A,B)
-  if(verbose) print(paste("Probability of B after A:",pAB))
-  pBC <- P_bigram_XC(B,C)
-  if(verbose) print(paste("Probability of B before C:",pBC))
-  pCorr <- pAB*pBC
-  if(verbose) print(paste("Probability the trigram is correct:",pCorr))
-  pIncorr <- (1-pAB)*(1-pBC)
-  if(verbose) print(paste("Probability the trigram is incorrect:",pIncorr))
-  pNorm <- pCorr/(pCorr+pIncorr)
-  return(pNorm)
+  freq <<- as.numeric(bigramDict[bigram])
+  if(is.na(freq)) return(0.1)
+  return(max(min(freq/n,0.9),0.1))
 }
 
 # Function that calculates the normalized probability that B comes after A
-P_bigram_norm <- function(A, B, verbose=FALSE){
-  print("Calculating...")
-  pB <- max(min(lemmasDict[B]/10,0.9),0.1)
+P_bigram <- function(A, B, verbose=FALSE){
+  if(verbose)print("Calculating...")
+  pB <- P_lemma(B)
   if(is.na(pB)) pB <- 0.1
   if(verbose) print(paste("Probability of B :",pB))
-  pAB <- P_bigram_lemma_AX(A,B)
+  pAB <- P_B_after_A(A,B)
   if(verbose) print(paste("Probability of B after A:",pAB))
   pCorr <- pB * pAB
   pIncorr <- (1-pB)*(1-pAB)
@@ -135,7 +85,10 @@ suggestWord <- function(word){
 # Select the suggestion that is likely to be correct (i.e. is common):
 selectSuggestion <- function(pw, word){
   require(stringdist)
-  adjust <- function(dist) return(1.7-0.8*dist)
+  adjust <- function(dist){
+    if(dist==1) return(1)
+    return(0.01)
+  }
   for(suggestion in names(pw)){
     pw[suggestion] <- pw[suggestion]*adjust(stringdist(suggestion,word))
   }
@@ -152,61 +105,99 @@ suggestLemma <- function(word){
 
 # Check if word is punctuation
 isPunct <- function(word){
-  return(grepl("[[:punct:]]",word))
+  return(grepl("[[:punct:][:digit:]]",word) | grepl("^[[:space:]]*$",word))
 }
 
 # Takes a word/tag/lemma csv, reads the words and makes
 # a decision for each word whether it's correct in it's context
 # If a word is deemed incorrect, it put's a suggestion in it's place.
-correctFile <- function(csvFile,destFile){
+correctFile <- function(csvFile,destFile,verbose=FALSE,extraVerbose=FALSE){
   require(stylo)
   rawData <- read.csv(csvFile)
   file_words <- as.character(rawData$Word)
   file_tags <- as.character(rawData$Tag)
   file_lemmas_raw <- as.character(rawData$Lemma)
   file_lemmas <- tolower(file_lemmas_raw)
+  file_length <- length(file_words)
   
   ##First we try to correct non-word errors in the file:
-  print("Non-word error pass...")
+  if(verbose)print("Non-word error pass...")
   possibleWord <- c()
   possibleLemma <- c()
-  for(i in 1:length(file_words)){
+  for(i in 1:file_length){
+    progress <- floor((i*100)/file_length)
+    percentage <- paste(formatC(progress, width=3, flag=" "),"%   ",sep="")
     word <- tolower(file_words[i])
-    freq <- dict[word]
-    correction <- file_words[i]
-    correctionLemma <- file_lemmas[i]
-    if(is.na(freq)){
-      suggestions <- suggestWord(word)
-      correction <- selectSuggestion(suggestions, word)
-      correctionLemma <- suggestLemma(correction)
-      print(paste(word,"is not a word, replacing with best guess:",correction))
+    if(isPunct(word)){
+      possibleLemma[i] <- file_lemmas[i]
+      possibleWord[i] <- file_words[i]
+    }else{
+      freq <- dict[word]
+      correction <- file_words[i]
+      correctionLemma <- file_lemmas[i]
+      if(is.na(freq)){
+        suggestions <- suggestWord(word)
+        correction <- selectSuggestion(suggestions, word)
+        correctionLemma <- suggestLemma(correction)
+        if(verbose)print(paste(percentage,word,"is not a word, replacing with best guess:",correction))
+      }
+      possibleLemma[i] <- correctionLemma
+      possibleWord[i] <- correction
     }
-    possibleLemma[i] <- correctionLemma
-    file_words[i] <- correction
   }
   
   ##Then we do a context based correction
-  print("Context error pass...")
-  correctWord <- c(file_words[1])
-  for(i in 2:length(file_words)){
-    print("Working...")
-    print(paste("calculating probability for:",paste(possibleLemma[i-1],possibleLemma[i])))
-    probCorrect <- P_bigram_norm(possibleLemma[i-1],possibleLemma[i],verbose = TRUE)
-    print(paste("result:",probCorrect))
-    verdict <- probCorrect>=0.5
-    print(paste("This word is correct:",verdict))
-    correction <- file_words[i]
-    correctionLemma <- possibleLemma[i]
-    if(!verdict){
-      suggestions <- suggestWord(possibleWord[i])
-      correction <- selectSuggestion(suggestions, possibleWord[i])
-      correctionLemma <- suggestLemma(correction)
-    } 
-    print(correction)
-    possibleLemma[i] <- correctionLemma
-    correctWord <- c(correctWord,correction)
+  if(verbose)print("Context error pass...")
+  startOfSentence <- TRUE
+  correctWord <- c()
+  firstWords <<- c()
+  for(i in 1:file_length){
+    progress <- floor((i*100)/file_length)
+    percentage <- paste(formatC(progress, width=3, flag=" "),"%   ",sep="")
+    if(startOfSentence){
+      correctWord[i] <- possibleWord[i]
+      firstWords <<- c(firstWords,possibleWord[i])
+      startOfSentence <- FALSE
+      if(verbose)print(paste(percentage,"================ NEW SENTANCE STARTING WITH",possibleWord[i],"============"))
+    }else if(isPunct(possibleWord[i])){
+      correctWord[i] <- possibleWord[i]
+      startOfSentence <- TRUE
+    }else{
+      if(verbose)print(paste(percentage,"Working..."))
+      if(verbose)print(paste(percentage,"calculating probability for:",paste(possibleLemma[i-1],possibleLemma[i])))
+      probCorrect <- P_bigram(possibleLemma[i-1],possibleLemma[i],extraVerbose)
+      if(verbose)print(paste(percentage,"result:",probCorrect))
+      verdict <- floor(probCorrect*10000) > 5000
+      if(verbose)print(paste(percentage,"This word is correct:",verdict))
+      correction <- possibleWord[i]
+      correctionLemma <- possibleLemma[i]
+      if(!verdict){
+        suggestions <- suggestWord(possibleWord[i])
+        correction <- selectSuggestion(suggestions, possibleWord[i])
+        correctionLemma <- suggestLemma(correction)
+        if(is.na(correction)){
+          correction <- possibleWord[i]
+          correctionLemma <- possibleLemma[i]
+        }
+      } 
+      if(verbose)print(paste(percentage,correction))
+      possibleLemma[i] <- correctionLemma
+      correctWord[i] <- correction
+    }
   }
   check <<- correctWord
   correctedData <- data.frame(Word=file_words,Tag=file_tags,Lemma=file_lemmas_raw,CorrectWord=correctWord)
   write.csv(correctedData, file=destFile)
+}
+
+testAccuracy <- function(csvCorrectedFile, csvCorrectFile) {
+  require(stylo)
+  ourData <- read.csv(csvCorrectedFile)
+  originalData <- read.csv(csvCorrectFile)
+  ourCorrections <- ourData$CorrectWord
+  orgCorrections <- originalData$CorrectWord
+  
+  correctCorrections <- ourCorrections[tolower(ourCorrections) == tolower(orgCorrections)]
+  accuracy <- length(correctCorrections)/length(ourCorrections)
+  return(accuracy)
 }
